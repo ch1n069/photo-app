ddabc97698f6bc05e9d5bb2320e6272d22b96c10
bruno <brunolalpachino@gmail.com>
2022-05-28 17:53:38 +0300

    created the fields for the models

--- photos/models.py
+++ photos/models.py
@@ -1,6 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Location(models.Model):
+    name = models.CharField(max_length=100, null=False)
+
+
+
+    def __str__(self):
+        return self.name
+
+
+
 
 
 class Category(models.Model):
@@ -14,6 +24,8 @@ class Category(models.Model):
 
 class  Photo(models.Model):
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=False)
+    location = models.ForeignKey(Location, on_delete=models.SET_NULL, null=False)
+    name = models.CharField(max_length=100, null=False)
     image = models.ImageField(null=False, blank=False)
     description = models.CharField(max_length=200, null=False)
 
