2c79b80044edfbd5b040d37d3c512abd49e3c0f7
bruno <brunolalpachino@gmail.com>
2022-05-29 08:07:25 +0300

    added the add photo route in url patterns

--- photos/urls.py
+++ photos/urls.py
@@ -6,6 +6,7 @@ from . import views
 urlpatterns = [
     path('', views.gallery, name='gallery'),
     path('photo/<str:pk>/', views.ViewPhoto, name='photo'),
+    path('add/', views.addPhoto, name='add')
 
 
 ]
