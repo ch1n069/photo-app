b5a9f25fb0e9e32076bdceee77063dda71b6376d
bruno <brunolalpachino@gmail.com>
2022-05-29 08:40:56 +0300

    ft == fetched the form data

--- photos/views.py
+++ photos/views.py
@@ -25,6 +25,24 @@ def ViewPhoto(request, pk):
 
 def addPhoto(request):
     categories = Category.objects.all()
+    if request.method == 'POST':
+        data = request.POST 
+        image  = request.FILES.get('image')
+
+        if data['category'] != 'none':
+            category = Category.objects.get(id=data['category'])
+        elif data['category_new'] != '':
+            category, created = category.objects.get_or_create(name=data['category_new'])
+        else:
+            category = None
+
+
+
+        photo = Photo.objects.create(
+            category=category,
+            description = data['description'],
+            image = image,
+        )
     ctx = {'categories':categories}
 
     return render (request , 'photos/add.html', ctx)
