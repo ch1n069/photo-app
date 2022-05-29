6791ce6e60547b055f6811cd2757cf5c38d1b8fe
bruno <brunolalpachino@gmail.com>
2022-05-29 15:55:53 +0300

    registered the route for search

--- photos/urls.py
+++ photos/urls.py
@@ -6,7 +6,8 @@ from . import views
 urlpatterns = [
     path('', views.gallery, name='gallery'),
     path('photo/<str:pk>/', views.ViewPhoto, name='photo'),
-    path('add/', views.addPhoto, name='add')
+    path('add/', views.addPhoto, name='add'),
+    path('search/', views.search_results, name='search_results'),
 
 
 ]
