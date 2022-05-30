46a22b9b0586594ee1f8081a343f11ae246b51f7
bruno <brunolalpachino@gmail.com>
2022-05-30 17:46:52 +0300

    deployment

--- photolab/settings.py
+++ photolab/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from pickle import FALSE
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-hyxkam$j+7+&lgcdt7x-$4js)6bngcyxuv=qognti&zytd_g4_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = FALSE
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['chino-photo-app.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
