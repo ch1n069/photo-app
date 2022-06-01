09743a09472b87a84a242ecc57ed4e45c523f831
bruno <brunolalpachino@gmail.com>
2022-06-01 10:02:05 +0300

    heroku deployment

--- photolab/wsgi.py
+++ photolab/wsgi.py
@@ -8,13 +8,12 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
-from whitenoise import WhiteNoise
 
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'photolab.settings.')
+os.environ['DJANGO_SETTINGS_MODULE'] = 'photolab.settings'
 
 application = get_wsgi_application()
-application = WhiteNoise(application)
 
